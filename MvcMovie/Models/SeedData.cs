24d57820419779d2ac414296b2cf3a168bb1f7a6
bdecoster <brecht.decoster@ctg.com>
2020-11-14 20:38:40 +0100

    Add project files.

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Rating = "PG-13",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Rating = "R",
+                        Price = 3.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "De Paniekzoaiers",
+                        ReleaseDate = DateTime.Parse("2020-10-16"),
+                        Genre = "Comedy",
+                        Rating = "R",
+                        Price = 14.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Last Born Son",
+                        ReleaseDate = DateTime.Parse("2020-10-30"),
+                        Genre = "Family",
+                        Rating = "PG-13",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Dummy Film",
+                        ReleaseDate = DateTime.Parse("2020-12-12"),
+                        Genre = "Horror",
+                        Rating = "R",
+                        Price = 4.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Dummy Film",
+                        ReleaseDate = DateTime.Parse("2020-12-12"),
+                        Genre = "Horror",
+                        Rating = "R",
+                        Price = 4.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Dummy Film",
+                        ReleaseDate = DateTime.Parse("2020-12-12"),
+                        Genre = "Horror",
+                        Rating = "R",
+                        Price = 4.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
